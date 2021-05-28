99abeccade19c0a73cf62ffbfa34b06a10698bf9
Duoc Nguyen <nguyenvanduocit@gmail.com>
2021-05-28 17:25:58 +0700

    feat: init interface

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-export const foo = 'baz'
+export interface PolvoClientOptions {}
+
+export class PolvoClient {
+  constructor(address: string, options: PolvoClientOptions = {}) {
+
+  }
+}
+
+export function createPolvoClient(address: string, options?: PolvoClientOptions) {
+  return new PolvoClient(address, options)
+}
